4a82021dff3f0d2fb280c6138010ccce17989214
Andrey <scootaloorainbow@yandex.ru>
2022-10-16 20:10:10 +0300

    feat: add modes for picker

--- src/DatePicker/index.tsx
+++ src/DatePicker/index.tsx
@@ -1,74 +1,158 @@
 import { DatePickerProps, DatePickerStyles } from "./DatePicker.typedef";
 import "./datePicker.css";
 import {
-    defaultDaysOfTheWeek,
-    getCurrentMonth, getFinalizedDates,
-    getFormattedDay,
-    getFormattedMonth,
-} from "../utils/dateHandlers"
-import {useEffect, useState} from "react";
+  defaultDaysOfTheWeek,
+  getCurrentMonth,
+  getFinalizedDates,
+  getFormattedDate,
+  getFormattedDay,
+  getFormattedMonth,
+} from "../utils/dateHandlers";
+import { useEffect, useState } from "react";
 import classNames from "classnames";
 
-
-const days = getFinalizedDates()
-
-const DatePicker = <T,>(props: DatePickerProps<T>) => {
-    const [currentMonth, setCurrentMonth] = useState(new Date().getMonth())
-    const [month, setMonth] = useState(days)
-    const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
-    const [selectedDate, setSelectedDate] = useState(new Date());
-    const initialsDates = getCurrentMonth({year: currentYear, month: currentMonth});
-const defaultStyles:DatePickerStyles = {
-header: {
-width: "100%",
-height: "350",
-position: "relative",
-},
-wrapper: {
+const days = getFinalizedDates();
+const defaultStyles: DatePickerStyles = {
+  header: {
+    width: "100%",
+    height: "350",
+    position: "relative",
+  },
+  wrapper: {
     display: "flex",
     flexWrap: "wrap",
     flexDirection: "column",
     width: "338px",
     padding: "24px",
-    border: "1px solid #1890FF", borderRadius: "16px"
-}, body: {
-    width: "338px"
-}, 
-activeDay: {
+    border: "1px solid #1890FF",
+    borderRadius: "16px",
+  },
+  body: {
+    width: "338px",
+  },
+  activeDay: {},
+};
 
-}
-}
-const getNextMonth = () => {
-    setCurrentMonth((prev) => {return  ++prev} )
-   setMonth(getFinalizedDates(new Date(), 10))
-    console.info(getFinalizedDates(new Date(), 10))
-}
-useEffect(() => {
-    console.info(getFinalizedDates(new Date(), 10))
-}, [currentMonth])
+type DatePickerMode = "single" | "partial" | "interval";
+type DatePickerInterval = { start: Date; end: Date };
+const DatePicker = <T,>(props: DatePickerProps<T>) => {
+  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
+  const [month, setMonth] = useState(days);
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+  const [selectedDates, setSelectedDates] = useState([
+    getFormattedDate(new Date()),
+  ]);
+  const [currentMode, setCurrentMode] = useState<DatePickerMode>("single");
+  const [datesInterval, setDatesInterval] = useState<DatePickerInterval>({
+    start: new Date(),
+    end: new Date(),
+  });
+  const isNumberOfMonthIsCorrect = currentMonth >= 0 && currentMonth <= 10;
+  const getNextMonth = () => {
+    const nextMonth = isNumberOfMonthIsCorrect ? currentMonth + 1 : 0;
+    setCurrentMonth(nextMonth);
+  };
+  const getPrevMonth = () => {
+    const prevMonth = isNumberOfMonthIsCorrect ? currentMonth - 1 : 0;
+    setCurrentMonth(prevMonth);
+  };
 
-    return (
-        <div style={defaultStyles.wrapper}>
-            <div className="header">
-                <time className={"datepicker-header__time"}>{getFormattedMonth(month[7])} {currentYear}</time>
-                <div className={"flex controls"}>
-                    <button className={"controller prev"} />
-                    <button className={"controller next"} onClick={getNextMonth} />
-                </div>
-            </div>
-            <ul className={"list flex row"}>
-                {defaultDaysOfTheWeek.map((item) => {
-                    return    <li key={item}>{item}</li>
-                })}
-            </ul>
-            <div className="body">
-                {month.map((item) =>
-                {
-                    const isDateNotRelatedToCurrentMonth = item.getMonth()  !== currentMonth
-                    return <button className={classNames("day", {"greyText":isDateNotRelatedToCurrentMonth})} key={item.toString()}>{getFormattedDay(item)}</button>})}
-            </div>
+  const doubleClickOnDay = () => {
+    setCurrentMode("partial");
+  };
+
+  const selectDay = (date: Date) => {
+    const formattedDate = getFormattedDate(date);
+    if (currentMode === "single") {
+      setSelectedDates([formattedDate]);
+    }
+    if (currentMode === "partial") {
+      if (selectedDates.includes(formattedDate)) {
+        const filteredDates = selectedDates.filter((item) => {
+          console.info(item, formattedDate);
+          return item !== formattedDate;
+        });
+        setSelectedDates(filteredDates);
+        return;
+      }
+      setSelectedDates((prev) => {
+        return [...prev, formattedDate];
+      });
+      if (selectedDates.length < 1) {
+        setCurrentMode("single");
+      }
+    }
+    if (currentMode === "interval") {
+      setSelectedDates((prev) => {
+        return [...prev, formattedDate];
+      });
+    }
+  };
+  useEffect(() => {
+    const month = getFinalizedDates(new Date(), currentMonth);
+    console.info(month);
+    setMonth(month);
+  }, [currentMonth]);
+
+  const downKeyEvent: React.KeyboardEventHandler<HTMLButtonElement> = (e) => {
+    const currentKey = e.key;
+    if (currentKey === "Shift") {
+      setCurrentMode("interval");
+    }
+  };
+  const hoverEvent: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    if (currentMode === "interval") {
+      selectDay(new Date(e.currentTarget.value));
+    }
+    console.info(e.currentTarget.value);
+  };
+  return (
+    <div style={defaultStyles.wrapper}>
+      <div className="header">
+        <time className={"datepicker-header__time"}>
+          {getFormattedMonth(month[7])} {currentYear}
+        </time>
+        <div className={"flex controls"}>
+          <button className={"controller prev"} onClick={getPrevMonth} />
+          <button className={"controller next"} onClick={getNextMonth} />
         </div>
-    )
-}
+      </div>
+      <ul className={"list flex row"}>
+        {defaultDaysOfTheWeek.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
+      <div className="body">
+        {month.map((item) => {
+          const isDateNotRelatedToCurrentMonth =
+            item.getMonth() !== currentMonth;
+          return (
+            <button
+              onClick={() => {
+                return selectDay(item);
+              }}
+              onKeyDown={downKeyEvent}
+              value={item.toString()}
+              onMouseEnter={(e) => {
+                return hoverEvent(e);
+              }}
+              onDoubleClick={doubleClickOnDay}
+              className={classNames(
+                "day",
+                {
+                  greyText: isDateNotRelatedToCurrentMonth,
+                },
+                { selected: selectedDates.includes(getFormattedDate(item)) }
+              )}
+              key={item.toString()}
+            >
+              {getFormattedDay(item)}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default DatePicker;
--- src/utils/dateHandlers.ts
+++ src/utils/dateHandlers.ts
@@ -3,78 +3,126 @@ import add from "date-fns/add";
 import sub from "date-fns/sub";
 
 type GetCurrentMonth = ({
-    year,
+  year,
+  month,
+}: {
+  year?: number;
+  month?: number;
+}) => Array<Date>;
+
+const availableMonths = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+export const getCurrentMonth: GetCurrentMonth = ({
+  year = new Date().getFullYear(),
+  month = new Date().getMonth(),
+}) => {
+  if (!availableMonths.includes(month)) {
+    throw new Error("Please, add month between 0 - 11");
+  }
+  const date = new Date(year, month, 1);
+
+  const dates = [];
+
+  while (date.getMonth() === month) {
+    dates.push(new Date(date));
+    date.setDate(date.getDate() + 1);
+  }
+
+  return dates;
+};
+export const getPreviousAndNextWeek = (
+  initialDate = new Date(),
+  month?: number,
+  year?: number
+) => {
+  const firstDay = new Date(
+    year ?? initialDate.getFullYear(),
+    month ?? initialDate.getMonth(),
+    1
+  );
+  const lastDayOfPrevMonth = new Date(
+    year ?? initialDate.getFullYear(),
+    month ?? initialDate.getMonth() - 1,
+    0
+  );
+  const lastDayOfMonth = new Date(
+    year ?? initialDate.getFullYear(),
+    month ?? initialDate.getMonth() + 1,
+    0
+  );
+  console.info(lastDayOfPrevMonth);
+  const previousWeek = [];
+
+  const nextWeek = [];
+  for (let i = 0; i < 7; i++) {
+    const result = add(lastDayOfMonth, { days: i });
+    nextWeek.push(result);
+  }
+  for (let i = 0; i < 7; i++) {
+    const result = sub(lastDayOfPrevMonth, { days: i });
+
+    previousWeek.push(result);
+  }
+  const monday = previousWeek.findIndex((item) => {
+    return item.getDay() === 1;
+  });
+
+  console.info(monday);
+  return {
+    previousWeek: previousWeek.reverse(),
+    nextWeek: nextWeek,
+  };
+};
+export const getFinalizedDates = (
+  initialDate = new Date(),
+  month?: number,
+  year?: number
+) => {
+  const { previousWeek, nextWeek } = getPreviousAndNextWeek(
+    initialDate,
     month,
-}:{year?: number, month?: number}) => Array<Date>;
+    year
+  );
+  const currentMonth = getCurrentMonth({
+    year: year ?? initialDate.getFullYear(),
+    month: month ?? initialDate.getMonth(),
+  });
+  const firstWeekOfCurrentMonth = currentMonth.slice(0, 7);
 
-const availableMonths = [0 ,1 , 2 , 3 , 4 , 5,6 , 7 , 8 , 9 , 10 , 11];
-
-export const getCurrentMonth:GetCurrentMonth = ({year = new Date().getFullYear(), month = new Date().getMonth()}) => {
-    if(!availableMonths.includes(month)){
-        throw new Error("Please, add month between 0 - 11")
-    }
-    const date = new Date(year, month, 1);
-
-    const dates = [];
-  
-    while (date.getMonth() === month) {
-      dates.push(new Date(date));
-      date.setDate(date.getDate() + 1);
-    }
-  
-    return dates;
-}
-export const getPreviousAndNextWeek = (initialDate = new Date(), month?:number, year?: number) => {
-    const firstDay = new Date(year ?? initialDate.getFullYear(), month ?? initialDate.getMonth(), 1);
-    const lastDayOfMonth = new Date(year ?? initialDate.getFullYear(),  month ??initialDate.getMonth()+1, 0);
-    const previousWeek = [];
-    const nextWeek = [];
-    for(let i = 0; i <= 7; i++){
-        const result = add(lastDayOfMonth, {days: i});
-        nextWeek.push(result)
-    }
-    for(let i = 0; i <=7; i++){
-        const result = sub(firstDay, {days: i})
-        previousWeek.push(result)
-    }
-    return {
-        previousWeek: previousWeek.reverse(), nextWeek: nextWeek
-    }
-}
-export const getFinalizedDates = (initialDate = new Date(), month?:number, year?:number) => {
-    const {previousWeek, nextWeek} = getPreviousAndNextWeek(initialDate, month, year);
-    const currentMonth = getCurrentMonth({year: year ?? initialDate.getFullYear(), month:  month ?? initialDate.getMonth()})
-    const firstWeekOfCurrentMonth = currentMonth.slice(0,7);
-
-    const updatedArray:Array<Date> = [];
-    firstWeekOfCurrentMonth.forEach((item, idx, arr) => {
-        const day = arr[idx];
-        if(idx === 0 && day.getDay() !== 1){
-            previousWeek.filter((item) => {
-                if(item.getDay() !== 0 && item.getDay() !== 6 ){
-                    updatedArray.push(item)
-                }
-            })
+  const updatedArray: Array<Date> = [];
+  firstWeekOfCurrentMonth.forEach((item, idx, arr) => {
+    const day = arr[idx];
+    if (idx === 0 && day.getDay() !== 1) {
+      previousWeek.filter((item) => {
+        if (item.getDay() !== 0 && item.getDay() !== 6) {
+          updatedArray.push(item);
         }
-    })
-    const temporaryArray = updatedArray.concat(currentMonth).concat(nextWeek)
-    const result = temporaryArray.filter((day, idx, array) => {
-        return (
-            array.findIndex((value) => {
-                return format(value, "MM dd yyyy") === format(day, "MM dd yyyy");
-            }) === idx
-        );
-    });
-    return     result
-
-}
+      });
+    }
+  });
+  const temporaryArray = updatedArray.concat(currentMonth).concat(nextWeek);
+  const result = temporaryArray.filter((day, idx, array) => {
+    return (
+      array.findIndex((value) => {
+        return format(value, "MM dd yyyy") === format(day, "MM dd yyyy");
+      }) === idx
+    );
+  });
+  return result;
+};
 export const getFormattedDay = (date: Date) => {
-return format(date, "dd")
-}
-
+  return format(date, "dd");
+};
+export const getFormattedDate = (date: Date | string) => {
+  const parsedToDate = new Date(date);
+  return format(parsedToDate, "dd.MM.yyyy");
+};
 export const getFormattedMonth = (month: Date, locale = "ru-RU") => {
-    const formattedMonth = month.toLocaleDateString (locale, {month: "long", day: undefined })
-    return formattedMonth[0].toUpperCase() + formattedMonth.slice(1)
-}
+  const formattedMonth = month.toLocaleDateString(locale, {
+    month: "long",
+    day: undefined,
+  });
+  return formattedMonth[0].toUpperCase() + formattedMonth.slice(1);
+};
 
-export const defaultDaysOfTheWeek = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"]
+export const defaultDaysOfTheWeek = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
