4659e3bfa85d579a06da5a230c3ae19b43aee127
Andrey <scootaloorainbow@yandex.ru>
2022-10-16 13:55:05 +0300

    feat: datepicker wip

--- src/DatePicker/index.tsx
+++ src/DatePicker/index.tsx
@@ -1,12 +1,26 @@
 import { DatePickerProps, DatePickerStyles } from "./DatePicker.typedef";
 import "./datePicker.css";
-import {getCurrentMonth, getFormattedDay, getFormattedMonth} from "../utils/dateHandlers"
+import NextIcon from "../../public/defaultIcons/next.svg";
+import PrevIcon from "../../public/defaultIcons/prev.svg";
+
+import {
+    defaultDaysOfTheWeek,
+    getCurrentMonth, getFinalizedDates,
+    getFormattedDay,
+    getFormattedMonth, getPreviousAndNextWeek,
+} from "../utils/dateHandlers"
+import {useEffect, useState} from "react";
+import classNames from "classnames";
 
 
+const days = getFinalizedDates()
 
 const DatePicker = <T,>(props: DatePickerProps<T>) => {
-
-    const initialsDates = getCurrentMonth();
+    const [currentMonth, setCurrentMonth] = useState(new Date().getMonth())
+    const [month, setMonth] = useState(days)
+    const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const initialsDates = getCurrentMonth({year: currentYear, month: currentMonth});
 const defaultStyles:DatePickerStyles = {
 header: {
 width: "100%",
@@ -17,22 +31,44 @@ wrapper: {
     display: "flex",
     flexWrap: "wrap",
     flexDirection: "column",
-    width: "400px",
+    width: "338px",
+    padding: "24px",
+    border: "1px solid #1890FF", borderRadius: "16px"
 }, body: {
-    width: "100%"
+    width: "338px"
 }, 
 activeDay: {
 
 }
 }
+const getNextMonth = () => {
+    setCurrentMonth((prev) => {return  ++prev} )
+   setMonth(getFinalizedDates(new Date(), 10))
+    console.info(getFinalizedDates(new Date(), 10))
+}
+useEffect(() => {
+    console.info(getFinalizedDates(new Date(), 10))
+}, [currentMonth])
 
     return (
         <div style={defaultStyles.wrapper}>
             <div className="header">
-                {getFormattedMonth(initialsDates[0])}
+                <time className={"datepicker-header__time"}>{getFormattedMonth(month[7])} {currentYear}</time>
+                <div className={"flex controls"}>
+                    <button className={"controller prev"} />
+                    <button className={"controller next"} onClick={getNextMonth} />
+                </div>
             </div>
+            <ul className={"list flex row"}>
+                {defaultDaysOfTheWeek.map((item) => {
+                    return    <li key={item}>{item}</li>
+                })}
+            </ul>
             <div className="body">
-                {initialsDates.map((item) => {return <button className={"day"}>{getFormattedDay(item)}</button>})}
+                {month.map((item) =>
+                {
+                    const isDateNotRelatedToCurrentMonth = item.getMonth()  !== currentMonth
+                    return <button className={classNames("day", {"greyText":isDateNotRelatedToCurrentMonth})} key={item.toString()}>{getFormattedDay(item)}</button>})}
             </div>
         </div>
     )
--- src/utils/dateHandlers.ts
+++ src/utils/dateHandlers.ts
@@ -1,5 +1,18 @@
 import format from "date-fns/format";
-export const getCurrentMonth = (year = new Date().getFullYear(), month = new Date().getMonth()) => {
+import add from "date-fns/add";
+import sub from "date-fns/sub";
+
+type GetCurrentMonth = ({
+    year,
+    month,
+}:{year?: number, month?: number}) => Array<Date>;
+
+const availableMonths = [0 ,1 , 2 , 3 , 4 , 5,6 , 7 , 8 , 9 , 10 , 11];
+
+export const getCurrentMonth:GetCurrentMonth = ({year = new Date().getFullYear(), month = new Date().getMonth()}) => {
+    if(!availableMonths.includes(month)){
+        throw new Error("Please, add month between 0 - 11")
+    }
     const date = new Date(year, month, 1);
 
     const dates = [];
@@ -11,11 +24,57 @@ export const getCurrentMonth = (year = new Date().getFullYear(), month = new Dat
   
     return dates;
 }
+export const getPreviousAndNextWeek = (initialDate = new Date(), month?:number, year?: number) => {
+    const firstDay = new Date(year ?? initialDate.getFullYear(), month ?? initialDate.getMonth(), 1);
+    const lastDayOfMonth = new Date(year ?? initialDate.getFullYear(),  month ??initialDate.getMonth()+1, 0);
+    const previousWeek = [];
+    const nextWeek = [];
+    for(let i = 0; i <= 7; i++){
+        const result = add(lastDayOfMonth, {days: i});
+        nextWeek.push(result)
+    }
+    for(let i = 0; i <=7; i++){
+        const result = sub(firstDay, {days: i})
+        previousWeek.push(result)
+    }
+    return {
+        previousWeek: previousWeek.reverse(), nextWeek: nextWeek
+    }
+}
+export const getFinalizedDates = (initialDate = new Date(), month?:number, year?:number) => {
+    const {previousWeek, nextWeek} = getPreviousAndNextWeek(initialDate, month, year);
+    const currentMonth = getCurrentMonth({year: year ?? initialDate.getFullYear(), month:  month ?? initialDate.getMonth()})
+    const firstWeekOfCurrentMonth = currentMonth.slice(0,7);
 
+    const updatedArray:Array<Date> = [];
+    firstWeekOfCurrentMonth.forEach((item, idx, arr) => {
+        const day = arr[idx];
+        if(idx === 0 && day.getDay() !== 1){
+            previousWeek.filter((item) => {
+                if(item.getDay() !== 0 && item.getDay() !== 6 ){
+                    updatedArray.push(item)
+                }
+            })
+        }
+    })
+    const temporaryArray = updatedArray.concat(currentMonth).concat(nextWeek)
+    const result = temporaryArray.filter((day, idx, array) => {
+        return (
+            array.findIndex((value) => {
+                return format(value, "MM dd yyyy") === format(day, "MM dd yyyy");
+            }) === idx
+        );
+    });
+    return     result
+
+}
 export const getFormattedDay = (date: Date) => {
 return format(date, "dd")
 }
 
 export const getFormattedMonth = (month: Date, locale = "ru-RU") => {
-return month.toLocaleDateString (locale, {month: "long", day: undefined })
+    const formattedMonth = month.toLocaleDateString (locale, {month: "long", day: undefined })
+    return formattedMonth[0].toUpperCase() + formattedMonth.slice(1)
 }
+
+export const defaultDaysOfTheWeek = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"]
