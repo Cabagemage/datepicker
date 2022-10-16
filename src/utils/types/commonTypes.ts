9044e4e1a70c334168dd607e140fe6b41124b314
Andrey <scootaloorainbow@yandex.ru>
2022-10-16 13:55:05 +0300

    feat: add types for common functions

---
+++ src/utils/types/commonTypes.ts
@@ -0,0 +1,37 @@
+export type GetCurrentMonth = ({
+  year,
+  month,
+}: {
+  year?: number;
+  month?: number;
+}) => Array<Date>;
+
+export type DateType = "string" | Date | null;
+
+export type GetDatesInRange = (start: DateType, end: DateType) => Array<Date>;
+
+type GetDatesOptions = {
+  initialDate?: Date;
+  month?: number;
+  year?: number;
+};
+
+type GetFormattedMonthParams = {
+  month: Date;
+  locale: Intl.LocalesArgument;
+  format?: "short" | "long";
+};
+
+export type GetFormattedMonthToLocale = (
+  params: GetFormattedMonthParams
+) => string;
+
+export type GetPreviousAndNextWeekForMonth = (args: GetDatesOptions) => {
+  nextWeek: Array<Date>;
+  previousWeek: Array<Date>;
+};
+export type GetFinalizedDatesArray = (params: GetDatesOptions) => Array<Date>;
+export type GetMonthsOfYear = (date: Date) => Array<Date>;
+
+export type GetFormattedShortDay = (date: Date) => string;
+export type GetFormattedDayToLocale = (date: Date | string) => string;
