1633f1111d17653e1c5bb912f889b3acfe51ba90
Andrey <scootaloorainbow@yandex.ru>
2022-10-19 22:29:24 +0300

    refactor: move constants to separate file

---
+++ src/utils/constants/constants.ts
@@ -0,0 +1,17 @@
+export const START_OF_NEW_MONTH_IDX = 7;
+export const ONE_YEAR = 1;
+export const ONE_MONTH = 1;
+export const MONDAY = 1;
+export const ONE_WEEK = 7;
+export const DECEMBER_NUMBER = 11;
+export const JANUARY_NUMBER = 0;
+export const MONTHS_IDX_LIST = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+export const DEFAULT_TRANSLATED_DAYS_OF_WEEK = [
+  "ПН",
+  "ВТ",
+  "СР",
+  "ЧТ",
+  "ПТ",
+  "СБ",
+  "ВС",
+];
