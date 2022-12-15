9b9ea6abc8e5b55ebb29067fbe0b97c27bf2ec31
Andrey <scootaloorainbow@yandex.ru>
2022-12-15 12:22:41 +0300

    fix: condition for isFirstDateEarlierThanSecondOne

--- src/core/handlers/isFirstDateEarlierThanSecondOne/index.ts
+++ src/core/handlers/isFirstDateEarlierThanSecondOne/index.ts
@@ -4,9 +4,9 @@ export const isFirstDateEarlierThanSecondOne = (
 ): boolean => {
   const endTime = new Date(end);
   const startTime = new Date(start);
-  const today = new Date().toLocaleDateString();
+
   return (
     startTime.getTime() <= endTime.getTime() &&
-    today !== startTime.toLocaleDateString()
+    endTime.toLocaleDateString() !== startTime.toLocaleDateString()
   );
 };
