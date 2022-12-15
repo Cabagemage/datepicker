2ee9894a7a17faf2f6aa49ac82d37d6fb63054db
Andrey <scootaloorainbow@yandex.ru>
2022-12-15 12:22:19 +0300

    fix:naming for getMonthCalendarViewDates

--- src/core/handlers/getMonthCalendarViewDates/index.ts
+++ src/core/handlers/getMonthCalendarViewDates/index.ts
@@ -1,29 +1,25 @@
-import {
-  GetMonthCalendarViewDates,
-} from "../../types/commonTypes";
+import { GetMonthCalendarViewDates } from "../../types/commonTypes";
 import { formatDate } from "../formatDate";
 import { MAX_DATES_LENGTH } from "../../constants";
 import { getCurrentMonth } from "../getCurrentMonth";
-import {getFirstWeekOfMonth} from "../getFirstWeekOfMonth";
-import {getLastWeekOfMonth} from "../getLastWeekOfMonth";
+import { getFirstWeekOfMonth } from "../getFirstWeekOfMonth";
+import { getLastWeekOfMonth } from "../getLastWeekOfMonth";
 
 export const getMonthCalendarViewDates: GetMonthCalendarViewDates = ({
   initialDate = new Date(),
   month,
   year,
 }) => {
-  const fiirstWeekOfMonth = getFirstWeekOfMonth({    initialDate,
-    month,
-    year,})
-  const lastWeekOfMonth = getLastWeekOfMonth({    initialDate,
-    month,
-    year,})
+  const firstWeekOfMonth = getFirstWeekOfMonth({ initialDate, month, year });
+  const lastWeekOfMonth = getLastWeekOfMonth({ initialDate, month, year });
   const currentMonth = getCurrentMonth({
     year: year ?? initialDate.getFullYear(),
     month: month ?? initialDate.getMonth(),
   });
 
-  const temporaryArray = fiirstWeekOfMonth.concat(currentMonth).concat(lastWeekOfMonth);
+  const temporaryArray = firstWeekOfMonth
+    .concat(currentMonth)
+    .concat(lastWeekOfMonth);
   const excludeRepeatedElements = temporaryArray.filter((day, idx, array) => {
     return (
       array.findIndex((value) => {
