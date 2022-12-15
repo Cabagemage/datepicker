e3b0d2d728eecdbd08be22045c1935390f950a10
Andrey <scootaloorainbow@yandex.ru>
2022-12-15 12:25:32 +0300

    refactor: rm default translated days of week

--- src/core/constants/constants.ts
+++ src/core/constants/constants.ts
@@ -7,13 +7,4 @@ export const DECEMBER_ORDINAL_NUMBER = 11;
 export const JANUARY_ORDINAL_NUMBER = 0;
 export const MONTHS_IDX_LIST = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 export const DAYS_IDX_LIST = [0, 1, 2, 3, 4, 5, 6];
-export const DEFAULT_TRANSLATED_DAYS_OF_WEEK = [
-  "ПН",
-  "ВТ",
-  "СР",
-  "ЧТ",
-  "ПТ",
-  "СБ",
-  "ВС",
-];
 export const MAX_DATES_LENGTH = [36, 43];
